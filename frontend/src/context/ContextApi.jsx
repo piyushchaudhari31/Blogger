e250f5aad09428cbf05d5ef8b08c49f5465e3189
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-14 22:42:36 +0530

    change url

--- frontend/src/context/ContextApi.jsx
+++ frontend/src/context/ContextApi.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 
 
 const ContextApi = (props) => {
-    const url = "http://localhost:3000"
+    const url = "https://blogger-bavz.onrender.com"
     const [user,setUser] = useState('')
     const [isEmailVarify , setIsEmailVaify] = useState(false)
 
