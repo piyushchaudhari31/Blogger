52dcf9e465e720427f2d5611fda696820a526418
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-14 22:34:05 +0530

    Update canvas.service.js

--- backend/src/service/canvas.service.js
+++ backend/src/service/canvas.service.js
@@ -3,21 +3,26 @@ const { createCanvas } = require("canvas");
 function generateInitialsAvatar(firstName, lastName) {
   const initials = `${firstName[0]}${lastName[0]}`.toUpperCase();
 
-  const canvas = createCanvas(200, 200);
-  const ctx = canvas.getContext("2d");
+  const svg = `
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="#000"/>
+      <text
+        x="50%"
+        y="55%"
+        text-anchor="middle"
+        dominant-baseline="middle"
+        font-size="100"
+        font-family="Arial"
+        font-weight="bold"
+        fill="#fff"
+      >
+        ${initials}
+      </text>
+    </svg>
+  `;
 
-  
-  ctx.fillStyle = "#000"; 
-  ctx.fillRect(0, 0, 200, 200);
-
-  
-  ctx.font = "bold 100px Arial";
-  ctx.fillStyle = "#FFFFFF";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText(initials, 100, 110);
-
-  return canvas.toDataURL(); // returns Base64 image
+  const base64 = Buffer.from(svg).toString("base64");
+  return `data:image/svg+xml;base64,${base64}`;
 }
 
 module.exports = generateInitialsAvatar;
