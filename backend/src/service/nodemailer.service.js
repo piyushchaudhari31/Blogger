f4a329dab7e426bf4bf89c232f6f4dc90edd1dc1
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-15 09:23:12 +0530

    update nodemailer

--- backend/src/service/nodemailer.service.js
+++ backend/src/service/nodemailer.service.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   port: 587,
   auth: {
     user: "9cd389001@smtp-brevo.com",
-    pass: "xsmtpsib-7040ac0a91e705fc3ebb414778bad58154170218d4a06e87d7b100d760dc12b4-ShkjuiqHU12za34H",
+    pass: "xsmtpsib-7040ac0a91e705fc3ebb414778bad58154170218d4a06e87d7b100d760dc12b4-QNQMmKE1HJ3SXFei",
   },
 });
 
