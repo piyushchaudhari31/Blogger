dc702901113482c6a85ab3bc385a7b01443380c8
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-24 14:16:55 +0530

    change code

--- backend/src/service/nodemailer.service.js
+++ backend/src/service/nodemailer.service.js
@@ -1,16 +1,13 @@
 const nodemailer = require("nodemailer");
 
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false,
   auth: {
-    user: "apikey", 
-    pass: process.env.USER_PASS, 
-  },
-  tls: {
-    rejectUnauthorized: false,
+    user: process.env.USER_LOGIN,
+    pass: process.env.USER_PASS,
   },
 });
 
-module.exports = transporter;
+module.exports = transporter
